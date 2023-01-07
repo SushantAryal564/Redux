26425bcf54fabdf9329ffb371a056eb31674c3f7
sushant <aryalsushant564@gmail.com>
2023-01-07 13:57:19 +0545

    Redux toolkit createSlice revisied

---
+++ redux_way/Counter.js
@@ -0,0 +1,41 @@
+import classes from "./Counter.module.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => {
+    return state.counter;
+  });
+  const show = useSelector((state) => state.showCounter);
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toogle" });
+  };
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <div>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={increaseHandler}>Decrease by 5</button>
+      </div>
+
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
