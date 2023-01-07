ef98a00f48af5cd561f6af41ff172dbc27d350ed
sushant <aryalsushant564@gmail.com>
2023-01-07 12:54:02 +0545

    Added Store and component to access the store value

---
+++ src/components/Counter.js
@@ -0,0 +1,36 @@
+import classes from "./Counter.module.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const toggleCounterHandler = () => {};
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <div>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={increaseHandler}>Decrease by 5</button>
+      </div>
+
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
